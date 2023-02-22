bfa23461400533bd6fb693a775a536de6fb7b0df
best <thebest_2425@hotmail.com>
2023-02-22 14:17:54 +0700

    add event formclosed to stop app

--- PLM/FormMain.Designer.cs
+++ PLM/FormMain.Designer.cs
@@ -995,6 +995,7 @@ namespace PLM
             this.Text = "Parliament";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.Shown += new System.EventHandler(this.FormMain_Shown);
             this.Resize += new System.EventHandler(this.FormMain_Resize);
